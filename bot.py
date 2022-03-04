47973545e28902630a2277a50f8ca8bc6c1aa566
gerardo10jose@gmail.com <jose_gerardo_guerrero@dlsu.edu.ph>
2022-03-04 15:32:40 +0800

    directory problems

--- bot.py
+++ bot.py
@@ -16,8 +16,13 @@ async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+try:
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            client.load_extension(f'cogs.{filename[:-3]}')
+except FileNotFoundError:
+    for filename in os.listdir('/cogs'):
+        if filename.endswith('.py'):
+            client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(os.getenv('TOKEN'))
